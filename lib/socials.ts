dc895e7f99444c815e0278eae8a0252497923aa0
ronload <91997734+ronload@users.noreply.github.com>
2026-07-29 10:49:29 +0800

    chore: add more alias for social media redirecting

--- lib/socials.ts
+++ lib/socials.ts
@@ -7,14 +7,19 @@ export interface SocialLink {
 }
 
 export const SOCIAL_LINKS: readonly SocialLink[] = [
-  { id: "github", label: "GitHub", url: "https://github.com/ronload", aliases: ["gh", "git"] },
+  {
+    id: "github",
+    label: "GitHub",
+    url: "https://github.com/ronload",
+    aliases: ["gh", "git", "code"],
+  },
   {
     id: "linkedin",
     label: "LinkedIn",
     url: "https://www.linkedin.com/in/luo-yong-neng",
     aliases: ["li", "in"],
   },
-  { id: "x", label: "X", url: "https://x.com/ron1oad", aliases: ["twitter"] },
+  { id: "x", label: "X", url: "https://x.com/ron1oad", aliases: ["twitter", "tweet", "tweets"] },
   {
     id: "instagram",
     label: "Instagram",
@@ -22,6 +27,11 @@ export const SOCIAL_LINKS: readonly SocialLink[] = [
     aliases: ["ig", "ins"],
   },
   { id: "threads", label: "Threads", url: "https://www.threads.com/@ynent" },
-  { id: "facebook", label: "Facebook", url: "https://www.facebook.com/RonTwps", aliases: ["fb"] },
+  {
+    id: "facebook",
+    label: "Facebook",
+    url: "https://www.facebook.com/RonTwps",
+    aliases: ["fb", "face", "book"],
+  },
   { id: "shopee", label: "Shopee", url: "https://shopee.tw/ronlo", archived: true },
 ];
